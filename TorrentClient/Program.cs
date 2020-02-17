128f5e3b935138841c02cc67e27d319585cc9ee5
Marcos Navarro <marcosok91@gmail.com>
2020-02-17 20:15:34 +0100

    Creada clase TorrentFile

--- TorrentClient/Program.cs
+++ TorrentClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using BencodeNET.Torrents;
+using BencodeNET.Parsing;
 
 namespace TorrentClient
 {
@@ -6,7 +9,17 @@ namespace TorrentClient
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Parse torrent by specifying the file path
+            var parser = new BencodeParser(); // Default encoding is Encoding.UTF8, but you can specify another if you need to
+            var torrent = parser.Parse<Torrent>("C:\\debian.torrent");
+
+            var torrentFile = new TorrentFile(
+                new List<byte>(torrent.OriginalInfoHashBytes),
+                new List<byte>(torrent.Pieces),
+                torrent.PieceSize,
+                torrent.TotalSize,
+                torrent.File.FileName
+            );
         }
     }
 }
---
+++ TorrentClient/TorrentFile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace TorrentClient
+{
+    public class TorrentFile
+    {
+        public List<byte> InfoHash { get; }
+        public List<byte> Pieces { get; }
+        public long PieceSize { get; }
+        public long TotalSize { get; }
+        public string Name { get; }
+
+        public TorrentFile(List<byte> infoHash, List<byte> pieces, long pieceSize, long totalSize, string name)
+        {
+            InfoHash = infoHash;
+            Pieces = pieces;
+            PieceSize = pieceSize;
+            TotalSize = totalSize;
+            Name = name;
+        }
+    }
+}
