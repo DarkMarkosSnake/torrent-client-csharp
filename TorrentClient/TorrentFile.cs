4787eed1fed84855ac33db6be98d9933960d4ddf
Marcos Navarro <marcosok91@gmail.com>
2020-02-17 21:47:32 +0100

    Añadido Announce a TorrentFile.cs

--- TorrentClient/TorrentFile.cs
+++ TorrentClient/TorrentFile.cs
@@ -4,14 +4,16 @@ namespace TorrentClient
 {
     public class TorrentFile
     {
+        public string Announce { get; }
         public List<byte> InfoHash { get; }
         public List<byte> Pieces { get; }
         public long PieceSize { get; }
         public long TotalSize { get; }
         public string Name { get; }
 
-        public TorrentFile(List<byte> infoHash, List<byte> pieces, long pieceSize, long totalSize, string name)
+        public TorrentFile(string announce, List<byte> infoHash, List<byte> pieces, long pieceSize, long totalSize, string name)
         {
+            Announce = announce;
             InfoHash = infoHash;
             Pieces = pieces;
             PieceSize = pieceSize;
